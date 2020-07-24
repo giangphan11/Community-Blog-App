d8888709560409d3812f5a535b1130d3251ed25c
Giang Phan <gphan485@gmail.com>
2020-07-24 13:09:40 +0700

    Runtime Permission Request & Pick Image form Gallery

--- app/src/main/java/phanbagiang/com/blogapp/Activities/RegisterActivity.java
+++ app/src/main/java/phanbagiang/com/blogapp/Activities/RegisterActivity.java
@@ -1,17 +1,36 @@
 package phanbagiang.com.blogapp.Activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.net.URI;
 
 import phanbagiang.com.blogapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
     Button regBtn;
+    ImageView regImage;
+    EditText regName,regMail, regPassword1, regPassword2;
     ProgressBar progressBar;
+
+    static int requestCode=113;
+
+    Uri uriPictureSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +41,27 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void addControls() {
+        regImage=findViewById(R.id.regImage);
+        regName=findViewById(R.id.regName);
+        regMail=findViewById(R.id.regEmail);
+        regPassword1=findViewById(R.id.regPassword1);
+        regPassword2=findViewById(R.id.regPassword2);
         regBtn=findViewById(R.id.regbtn);
         progressBar=findViewById(R.id.progressBar);
     }
     private void addEvents(){
+        regImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT>=22){
+                    checkAndRequestForPermission();
+                }
+                else{
+                    openGallery();
+                }
+            }
+        });
+
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,5 +69,51 @@ public class RegisterActivity extends AppCompatActivity {
                 regBtn.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    private void openGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, requestCode);
+    }
+
+    private void checkAndRequestForPermission() {
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(this, "Please accept the required for permission!", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(RegisterActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        requestCode);
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }else{
+            openGallery();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==requestCode && resultCode==RESULT_OK &&data!=null){
+            uriPictureSelected=data.getData();
+            regImage.setImageURI(uriPictureSelected);
+        }
     }
 }
