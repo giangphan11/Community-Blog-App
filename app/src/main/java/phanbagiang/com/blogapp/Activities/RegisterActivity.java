a29167bf83be70f867e7215a7d1316d657700e46
Giang Phan <gphan485@gmail.com>
2020-07-24 12:33:16 +0700

    firebase setup

--- app/src/main/java/phanbagiang/com/blogapp/Activities/RegisterActivity.java
+++ app/src/main/java/phanbagiang/com/blogapp/Activities/RegisterActivity.java
@@ -3,14 +3,35 @@ package phanbagiang.com.blogapp.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import phanbagiang.com.blogapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button regBtn;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        addControls();
+        addEvents();
+    }
+
+    private void addControls() {
+        regBtn=findViewById(R.id.regbtn);
+        progressBar=findViewById(R.id.progressBar);
+    }
+    private void addEvents(){
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                regBtn.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
